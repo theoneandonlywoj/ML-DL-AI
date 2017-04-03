179a74161681dd44c2fcb7afdbb65f2ef88afb6b
arcyfelix <wor@kth.se>
2017-04-03 23:32:11 +0200

    Tested and functional version

--- VGG/train.py
+++ VGG/train.py
@@ -10,7 +10,7 @@ x_train, x_val, y_train, y_val = get_data_MNIST()
 # Recalling the network defined in network.py
 model = ANN(WIDTH = 28, HEIGHT = 28, CHANNELS = 1, LABELS = 10)
 # Training
-model.fit(x_train, y_train, n_epoch = 1000, validation_set = (x_val, y_val), 
+model.fit(x_train, y_train, n_epoch = 1, validation_set = (x_val, y_val), 
 show_metric = True, batch_size = 200, shuffle = True, #snapshot_step = 100,
       snapshot_epoch = True, run_id = 'VGG')
 
